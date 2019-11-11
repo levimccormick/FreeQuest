d4e97cdf7e2cfa8bef0e82af8f1bc6696256e358
levimccormick <levi@levimccormick.com>
2019-11-11 00:18:44 +0000

    Adding testing environment.

--- code/test_heroes.py
+++ code/test_heroes.py
@@ -4,10 +4,25 @@ import combat
 def test_warrior():
     """ Set of tests to see if the warrior's abilities balance. """
     warrior = combat.combatant(8, 4, 2)
-
+    
+    print("Fighting weak monster.")
     victories = 0
     while warrior.health > 0 and victories < 1000:
         weak_monster = combat.combatant(1, 1, 2)
         victories += combat.run_combat(warrior, weak_monster)
-
     print("Warrior won {} times.".format(victories))
+    
+    print("Fighting strong monster.")
+    victories = 0
+    while warrior.health > 0 and victories < 1000:
+        strong_monster = combat.combatant(3, 2, 2)
+        victories += combat.run_combat(warrior, strong_monster)
+    print("Warrior won {} times.".format(victories))
+
+def main():
+    """ Testing heroes. """
+    print("Testing warrior...")
+    test_warrior()
+
+if __name__ == '__main__':
+    main()
